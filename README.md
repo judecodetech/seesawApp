062663f2bfe8eb5ed349b7a197286108d70c019f
Jude Ugbefu <judeconnectionz@gmail.com>
2016-10-10 04:23:01 +0200

    Seesaw basic API App

---
+++ README.md
@@ -0,0 +1,23 @@
+# seesawApp
+
+1. This is a Ruby application so please make sure Ruby is installed on your machine.
+		run **_brew install ruby_** on Mac or **_apt-get install ruby_** on Linux(Ubuntu)
+
+2. git clone this repo to your local machine
+
+3. run command - **_gem install bundler_**'
+
+4. run command - **_bunle install_**'
+
+5. cd into the root folder of the project - **_cd seesaw_**
+
+6. run command **_ruby seesaw.rb_** to run the application
+
+7. After running the last command above, navigate to the root
+folder of the project and a file called **output.csv** will
+be present. 
+
+
+
+**This app is executed from the console. It makes a request to a given API Endpoint
+and outputs the response as a CSV file.**
---
+++ seesaw.rb
@@ -0,0 +1,48 @@
+# require libaries
+require 'csv'
+require 'httparty'
+require 'json'
+ 
+
+username = 'testAPI'
+password = 'testAPI'
+
+api_url = 'platform1.greenseesaw.com/seesaw_api_test/_design/main/_view/allDocs'
+
+endpoint = "http://#{username}:#{password}@#{api_url}"
+response = HTTParty.get(endpoint) # make get request to endpoint
+json_data = JSON.parse(response.body) # parse json data from response
+
+# Set options for CSV processing
+options = {   col_sep: ' | ', 
+			  headers: true, 
+			  row_sep: "\n\n",
+			  :converters => lambda do |field|
+             	field.to_s.tr('"','')
+           	  end
+       	   }
+
+# Open CSV file for writing
+CSV.open("output.csv", "w", options) do |csv|
+
+	# Set row headers for CSV file
+	csv << ['id', 'rev', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'SubObj.Qest', 'SubObj.Ans']
+
+	# Loop through json data from api endpoint
+	json_data['rows'].each do |data|
+
+		# Cater for sub-objects
+		subObjects = {}
+		data['value'].each do |key, value|
+			if key == 'SubObj'
+				subObjects = value
+			end
+		end
+
+		# write rows to CSV
+		csv << [  data['value']['_id'], data['value']['_rev'], data['value']['col1'],
+				  data['value']['col2'], data['value']['col3'], data['value']['col4'],
+				  data['value']['col5'], data['value']['col6'], subObjects['a'], subObjects['b']
+				]
+	end
+end
